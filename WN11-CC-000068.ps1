d347968575d14380e9be62aba5c194f3f3f76957
Sangwon Yoon <syoon1742@outlook.com>
2026-05-28 16:05:59 -0700

    Create WN11-CC-000068.ps1

---
+++ WN11-CC-000068.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the use of a hardware security device with Windows Hello for Business is enabled.
+
+.NOTES
+    Author          : Sangwon Yoon
+    LinkedIn        : linkedin.com/in/sangwon-yoon/
+    GitHub          : github.com/sangwon-yoon
+    Date Created    : 2026-05-28
+    Last Modified   : 2026-05-28
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000068
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000068/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    PS C:\> .\WN11-CC-000068.ps1
+#>
+
+$regPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation'
+
+If (!(Test-Path $regPath)) {
+    New-Item -Path $regPath -Force
+}
+
+Set-ItemProperty -Path $regPath -Name 'AllowProtectedCreds' -Value 1 -Type DWord
+
+# Apply the changes
+gpupdate /force
